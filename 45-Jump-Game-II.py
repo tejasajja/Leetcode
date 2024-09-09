e8bd228fade0dce9bb49d426226955142f3b78d5
Teja Sajja <112130561+tejasajja@users.noreply.github.com>
2024-09-09 11:41:41 -0700

    [45] [Time Beats: 86.57%] [Memory Beats: 76.07%] - LeetPush

--- 45-Jump-Game-II.py
+++ 45-Jump-Game-II.py
@@ -4,30 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         \\\
-        # max_index = 0
-        # for i in range(len(nums)):
-        #     if i > max_index:
-        #         break
-        #     max_index = max(max_index, i + nums[i])
-        #     while j
-        # return True
-        n = len(nums)
-        if n ==1:
-            return 0
-        cur = 0
-        jump= 0
-        maxReach = 0
-        maxReachidx = 0
+        # n = len(nums)
+        # if n ==1:
+        #     return 0
+        # cur = 0
+        # jump= 0
+        # maxReach = 0
+        # maxReachidx = 0
 
-        for i in range(n):
-            if (cur + nums[cur] >= n-1):
-                return jump+1
-            if i > cur + nums[cur] :
-                cur =  maxReachidx
-                jump+=1
-            reach = i+nums[i]
-            if reach>maxReach:
-                maxReach = reach
-                maxReachidx = i
-        return jump+1
-
+        # for i in range(n):
+        #     if (cur + nums[cur] >= n-1):
+        #         return jump+1
+        #     if i > cur + nums[cur] :
+        #         cur =  maxReachidx
+        #         jump+=1
+        #     reach = i+nums[i]
+        #     if reach>maxReach:
+        #         maxReach = reach
+        #         maxReachidx = i
+        # return jump+1
+        res =0 
+        l = r =0
+        while r < len(nums)-1:
+            farthest = 0 
+            for i in range(l,r +1):
+                farthest = max(farthest, i + nums[i])
+            l = r+1
+            r = farthest
+            res +=1
+        return res
