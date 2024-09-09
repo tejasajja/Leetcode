4577ac79128caa695374bb554b449b4046ca4aba
Teja Sajja <112130561+tejasajja@users.noreply.github.com>
2024-09-09 10:34:37 -0700

    [75] [Time Beats: 15.18%] [Memory Beats: 69.76%] - LeetPush

---
+++ 75-Sort-Colors.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(1,len(nums)):
+            key = nums[i]
+
+            j = i-1 
+
+            while j>=0 and nums[j]>key:
+                nums[j+1] = nums[j]
+                j-=1
+            nums[j+1] = key
+
+        return nums
