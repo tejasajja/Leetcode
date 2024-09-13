302f1ec5a1928f2e027e43db8c2f50a43ddfb40e
Teja Sajja <112130561+tejasajja@users.noreply.github.com>
2024-09-12 19:01:14 -0700

    [94] [Time Beats: 60.15%] [Memory Beats: 85.48%] - LeetPush

---
+++ 94-Binary-Tree-Inorder-Traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        def rec(root):
+            if root==None:
+                return []
+            return rec(root.left)+[root.val]+rec(root.right)
+        return rec(root)
+
+        
