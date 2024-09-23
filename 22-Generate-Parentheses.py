e98137777c93ab2f906eafff167fc91394d389d0
Teja Sajja <112130561+tejasajja@users.noreply.github.com>
2024-09-23 15:13:36 -0700

    [22] [Time Beats: 60.82%] [Memory Beats: 66.00%] - LeetPush

--- 22-Generate-Parentheses.py
+++ 22-Generate-Parentheses.py
@@ -4,23 +4,35 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        res =[]
-        stack =[]
+        # res =[]
+        # stack =[]
 
-        def backtrack (openN, closedN):
-            if openN == closedN ==n:
-                res.append(''.join(stack))
+        # def backtrack (openN, closedN):
+        #     if openN == closedN ==n:
+        #         res.append(''.join(stack))
+        #         return 
+
+        #     if openN < n:
+        #         stack.append('(')
+        #         backtrack(openN + 1,closedN)
+        #         stack.pop()
+
+        #     if closedN < openN:
+        #         stack.append(')')
+        #         backtrack(openN,closedN + 1)
+        #         stack.pop()
+
+        # backtrack(0,0)
+        # return res
+        com = []
+        def backtracking(o,c, path):
+            if o > n or c >n or o < c:
                 return 
+            if o ==n and c == n:
+                com.append(path)
+                return
+            backtracking(o +1 , c , path+'(')
+            backtracking(o , c +1  ,path+')')
 
-            if openN < n:
-                stack.append('(')
-                backtrack(openN + 1,closedN)
-                stack.pop()
-
-            if closedN < openN:
-                stack.append(')')
-                backtrack(openN,closedN + 1)
-                stack.pop()
-
-        backtrack(0,0)
-        return res
+        backtracking(0,0,'')
+        return com
