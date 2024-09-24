3f1a88a7027847266ed908df71741adef9df5da4
Teja Sajja <112130561+tejasajja@users.noreply.github.com>
2024-09-24 12:15:53 -0700

    [39] [Time Beats: 98.37%] [Memory Beats: 11.42%] - LeetPush

--- 39-Combination-Sum.py
+++ 39-Combination-Sum.py
@@ -1,22 +1,19 @@
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res =[]
-        
-        subset = []
+        ans = []
+        ds = []
 
-        def dfs (i, subset,total):
-            if total == target:
-                res.append(subset.copy())
-                return 
-            if i>=len(candidates) or total >target:
+
+        def findCombination(ind: int, target: int):
+            if ind == len(candidates):
+                if target == 0:
+                    ans.append(ds[:])
                 return
-
-            subset.append(candidates[i])
-            dfs(i, subset, total+candidates[i] )
-
-            subset.pop()
-            
-            dfs(i+1,subset,total)
-
-        dfs(0,[],0)
-        return res
+            if candidates[ind] <= target:
+                ds.append(candidates[ind])
+                findCombination(ind, target - candidates[ind])
+                ds.pop()
+            findCombination(ind + 1, target)
+        findCombination(0, target)
+        return ans
